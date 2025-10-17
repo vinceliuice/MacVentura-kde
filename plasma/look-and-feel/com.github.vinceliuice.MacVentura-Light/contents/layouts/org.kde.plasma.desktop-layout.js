8ffce998a143b2d7b700203c1e6f26bb8bfd5f36
vinceliuice <vinceliuice@hotmail.com>
2025-10-17 22:03:33 +0800

    update

--- plasma/look-and-feel/com.github.vinceliuice.MacVentura-Light/contents/layouts/org.kde.plasma.desktop-layout.js
+++ plasma/look-and-feel/com.github.vinceliuice.MacVentura-Light/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,6 +1,50 @@
-loadTemplate("org.github.desktop.MacOSPanel")
+var panel = new Panel
+var panelScreen = panel.screen
 
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+// No need to set panel.location as ShellCorona::addPanel will automatically pick one available edge
+
+// For an Icons-Only Task Manager on the bottom, *3 is too much, *2 is too little
+// Round down to next highest even number since the Panel size widget only displays
+// even numbers
+panel.height = 2 * Math.floor(gridUnit * 2.5 / 2)
+panel.location = "top"
+
+// Restrict horizontal panel to a maximum size of a 21:9 monitor
+const maximumAspectRatio = 21/9;
+if (panel.formFactor === "horizontal") {
+    const geo = screenGeometry(panelScreen);
+    const maximumWidth = Math.ceil(geo.height * maximumAspectRatio);
+
+    if (geo.width > maximumWidth) {
+        panel.alignment = "center";
+        panel.minimumLength = maximumWidth;
+        panel.maximumLength = maximumWidth;
+    }
 }
+
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+
+var bpanel = new Panel
+bpanel.location = "bottom"
+bpanel.lengthMode = "fit"
+bpanel.hiding = "dodgewindows"
+bpanel.height = 64
+
+let taskBar = bpanel.addWidget("org.kde.plasma.icontasks")
+taskBar.currentConfigGroup = ["General"]
+taskBar.writeConfig("launchers", [
+    "preferred://filemanager",
+    "preferred://browser",
+    "applications:org.kde.konsole.desktop",
+    "applications:systemsettings.desktop",
+])
+panel.addWidget("org.kde.plasma.appmenu")
+panel.addWidget("org.kde.plasma.panelspacer")
+panel.addWidget("org.kde.plasma.marginsseparator")
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.marginsseparator")
+panel.addWidget("org.kde.plasma.digitalclock")
+panel.addWidget("org.kde.plasma.showdesktop")
+
